ac5c90ae77755e3195adbae6dbff0ead8b27d746
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-09-15 11:52:32 -0300

    change: push_back to delegate reallocation to reallocate member function in MySexyVector

--- TastyAndSexyIterators/MySexyVector.cpp
+++ TastyAndSexyIterators/MySexyVector.cpp
@@ -35,21 +35,12 @@ int MySexyVector::capacity() const
 void MySexyVector::push_back(int value)
 {
 	if (currentCapacity == 0) {
-		delete[] container;
-		container = new int[1];
-		currentCapacity = 1;
+		reallocate(1);
 	}
 	else if (currentSize == currentCapacity) {
-		const int newCapacity = currentCapacity * 2;
-		int* newContainer = new int[newCapacity];
-		for (int i = 0; i < currentCapacity; i++) {
-			newContainer[i] = container[i];
-		}
-		delete[] container;
-		container = newContainer;
-		currentCapacity = newCapacity;
+		reallocate(currentCapacity * 2);
 	}
-
+	std::cout << "Pushing back " << value << std::endl;
 	container[currentSize] = value;
 	currentSize++;
 }
