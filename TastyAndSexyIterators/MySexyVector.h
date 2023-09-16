c7e6db6b0d6be03c72e0b843e274ce086ce0fb89
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2023-09-16 19:39:21 -0300

    implement: a sexy iterator for my sexy vector

--- TastyAndSexyIterators/MySexyVector.h
+++ TastyAndSexyIterators/MySexyVector.h
@@ -1,6 +1,72 @@
 #pragma once
 
 class MySexyVector {
+
+public:
+	class my_sexy_iterator {
+	public:
+		my_sexy_iterator(int* ptr)
+			:
+			ptr(ptr)
+		{ }
+
+		my_sexy_iterator& operator++()
+		{
+			ptr++;
+			return *this;
+		}
+		my_sexy_iterator operator++(int)
+		{
+			my_sexy_iterator copy = *this;
+			ptr++;
+			return copy;
+		}
+
+		my_sexy_iterator& operator--()
+		{
+			ptr--;
+			return *this;
+		}
+		my_sexy_iterator operator--(int)
+		{
+			my_sexy_iterator copy = *this;
+			ptr--;
+			return copy;
+		}
+
+		my_sexy_iterator& operator+=(int value) {
+			for (int i = 0; i < value; i++) {
+				ptr++;
+			}
+			return *this;
+		}
+
+		my_sexy_iterator& operator-=(int value)
+		{
+			for (int i = 0; i < value; i++) {
+				ptr--;
+			}
+			return *this;
+		}
+
+		int operator*() const
+		{
+			return *ptr;
+		}
+		bool operator==(const my_sexy_iterator& other) const
+		{
+			return ptr == other.ptr;
+		}
+		bool operator!=(const my_sexy_iterator& other) const
+		{
+			return !(*this == other);
+		}
+	private:
+		int* ptr = nullptr;
+	};
+public:
+	typedef int value_type;
+	typedef my_sexy_iterator iterator;
 public:
 	MySexyVector() = default;
 	MySexyVector(const MySexyVector& source);
@@ -19,7 +85,16 @@ public:
 	void clear();
 	void push_back(int value);
 	void pop_back();
-	void resize(int count);
+	//void resize(int count);
+	// iterators functions
+	my_sexy_iterator begin() const
+	{
+		return my_sexy_iterator(container);
+	}
+	my_sexy_iterator end() const
+	{
+		return my_sexy_iterator(container + currentSize);
+	}
 
 private:
 	void reallocate(int new_cap);
